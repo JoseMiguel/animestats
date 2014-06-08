52e8ad9a080213f2fb0f36593a4b8d826a0c8a80
jmj <jmj.noblecilla@gmail.com>
2014-06-08 00:03:49 -0300

    adding reviews; testing last season

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
-from anime import Anime
+from statistics import *
 import urllib.request
+import operator
+from anime import Anime
 from bs4 import BeautifulSoup
 from collections import defaultdict
 
@@ -7,6 +9,7 @@ class AnimeParser:
 	def __init__(self):
 		self.ann_base_url = "http://www.animenewsnetwork.com"
 		self.ann_rank_url = "http://www.animenewsnetwork.com/encyclopedia/ratings-anime.php?top50=best_bayesian&n=1000"
+		self.ann_preview = "http://www.animenewsnetwork.com/preview-guide/2014/the-spring-anime"
 
 	def getAllRank(self):
 		html = urllib.request.urlopen(self.ann_rank_url)
@@ -34,8 +37,40 @@ class AnimeParser:
 			date = str(vintageDiv.find('span').text)
 			anime.year = int(date[:4])
 
-		
-def run():
+	def getPreview(self):
+		html = urllib.request.urlopen(self.ann_preview)
+		soup = BeautifulSoup(html)
+		animeTable = soup.find('table')
+		newAnimes = []
+		for tr in animeTable.findAll('td'):
+			name = (tr.find('b').text)
+			url =  str(tr.find('a',href=True)['href'])
+			html = urllib.request.urlopen(self.ann_base_url + url)
+			review = BeautifulSoup(html)
+			ratings = []
+			for p in review.findAll('p'):
+				ratingStr = p.text[:50]
+				if ratingStr.find('Rating:') != -1:
+					for t in ratingStr.split():
+						try:
+							ratings.append(float(t))
+							break
+						except:
+							pass
+			if ( ratings ):
+				newAnimes.append([mean(ratings),name,ratings])
+
+		sorted(newAnimes)
+		for anime in newAnimes:
+			ratings = anime[2]
+			if len(ratings) >= 2:
+				std = stdev(ratings)
+			else:
+				std = .0
+			print(anime[0],'|', anime[1], std)
+			
+
+def runRank():
 	ap = AnimeParser()
 	animeList = ap.getAllRank()
 	for anime in animeList:
@@ -47,11 +82,16 @@ def run():
 		bucket[x.year].append(x)
 
 	for year, animes in bucket.items():
-		filePerYear = open(str(year)+".csv", 'w')
+		filePerYear = open(str(year)+".tsv", 'w')
 		filePerYear.write("Rank,Name,URL\n")
 		for anime in animes:
-			filePerYear.write(str(anime.rank) + "," + anime.name+ "," + anime.url +"\n")
+			filePerYear.write(str(anime.rank) + "\t" + anime.name+ "\t" + anime.url +"\n")
 
 		filePerYear.close()
 
-run()
+def runPreview():
+	ap = AnimeParser()
+	ap.getPreview()
+
+
+runPreview()
